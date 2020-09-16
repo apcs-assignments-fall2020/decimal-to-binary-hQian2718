62fde9d1e82769401ab7dea56f64a7801e90be61
hQian2718 <22kqian@athenian.org>
2020-09-16 08:29:11 +0000

    Wrote Binary to Decimal Method

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,34 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
+        int pow = 0;
+        int sum = 0;
+        for(int i = binary.length() - 1; i >= 0; i --){
+            if(binary.charAt(i) == '0'){
+            }else if(binary.charAt(i) == '1'){
+                sum += Math.pow(2,pow);
+            }else{
+                return -1;
+            }
+            pow ++;
+        }
         // REPLACE WITH YOUR CODE HERE
+        return sum;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        String b = null;
+        int d = -1;
+        do{
+            System.out.println("Enter a positive number in binary format!");
+            b = scan.next();
+            d = binaryToDecimal(b);
+        }
+        while(d == -1);
+
+        System.out.println("Your binary number is equal to " + binaryToDecimal(b) + " in decimal." );
+        scan.close();
     }
 }
